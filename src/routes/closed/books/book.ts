327be4af0c196e5ef10ede2255554152b01db468
daltonmilti <147172231+daltonmilti@users.noreply.github.com>
2024-10-29 15:46:18 -0700

    Finalized documentation for book routes

--- src/routes/closed/books/book.ts
+++ src/routes/closed/books/book.ts
@@ -1,4 +1,9 @@
 /**
+ * @apiDefine DBError
+ * @apiError (500: Database Error) {String} message "Error on Server Side"
+ */
+
+/**
  * @api {get} /book/all Request to get all book
  * @apiName GetAllbook
  * @apiGroup book
@@ -6,6 +11,7 @@
  * @apiSuccess {Object[]} entry the book object containing information of the book <code>name</code>
  *
  * @apiError (500 Internal Server Error) {String} message "Internal Server Error"
+ * @apiUse DBError
  */
 
 /**
@@ -16,8 +22,8 @@
  * @apiSuccess {Object[]} entry the book object containing information of the book from author <code>author</code>
  * @apiParam {string} author the author to look up.
  *
- * @apiError (500 Internal Server Error) {String} message "Internal Server Error"
  * @apiError (404: Name Not Found) {string} message "Author not found"
+ * @apiUse DBError
  */
 
 /**
@@ -28,8 +34,8 @@
  * @apiSuccess {Object[]} entry the book object containing information of the book from author <code>author</code>
  * @apiParam {string} isbn the isbn to look up.
  *
- * @apiError (500 Internal Server Error) {String} message "Internal Server Error"
  * @apiError (404: Name Not Found) {string} message "Author not found"
+ * @apiUse DBError
  */
 
 /**
@@ -40,8 +46,8 @@
  * @apiSuccess {Object[]} entry the book object containing information of the book from ISBN <code>author</code>
  * @apiParam {string} ISBN the ISBN to look up.
  *
- * @apiError (500 Internal Server Error) {String} message "Internal Server Error"
  * @apiError (404: Name Not Found) {string} message "Author not found"
+ * @apiUse DBError
  */
 
 /**
@@ -52,8 +58,8 @@
  * @apiSuccess {Object[]} entry the book object containing information of the book from title <code>author</code>
  * @apiParam {string} title the title to look up.
  *
- * @apiError (500 Internal Server Error) {String} message "Internal Server Error"
  * @apiError (404: Name Not Found) {string} message "Author not found"
+ * @apiUse DBError
  */
 
 /**
@@ -64,8 +70,8 @@
  * @apiSuccess {Object[]} entry the book object containing information of the book from title <code>author</code>
  * @apiParam {string} rating the rating to look up.
  *
- * @apiError (500 Internal Server Error) {String} message "Internal Server Error"
  * @apiError (404: Name Not Found) {string} message "Author not found"
+ * @apiUse DBError
  */
 
 /**
@@ -83,58 +89,92 @@
  *
  * @apiError (400: Invalid Priority) {String} message "Invalid or missing Priority  - please refer to documentation"
  * @apiError (404: No messages) {String} message "No Priority <code>priority</code> messages found"
+ * @apiUse DBError
  */
 
 /**
- * @api {post} /message Request to add an entry
+ * @api {post} /newBook Request to add a new book
  *
- * @apiDescription Request to add a message and someone's name to the DB
+ * @apiDescription Request to add a new book with title, author, published date, ISBN, and an optional message.
  *
- * @apiName PostMessage
- * @apiGroup Message
+ * @apiName PostBook
+ * @apiGroup book
  *
- * @apiBody {string} name someone's name *unique
- * @apiBody {string} message a message to store with the name
- * @apiBody {number} priority a message priority [1-3]
+ * @apiBody {string} title the new book title
+ * @apiBody {string} author the new book author
+ * @apiBody {string} date the new published date
+ * @apiBody {number} isbn the new isbn
+ * @apiBody {string} [message] An optional message or note associated with the book
  *
- * @apiSuccess (Success 201) {String} entry the string:
- *      "{<code>priority</code>} - [<code>name</code>] says: <code>message</code>"
+ * @apiSuccess (201: Created) {Object} entry the details of the newly created book entry
+ * @apiSuccess (201: Created) {String} entry.title Title of the new book
+ * @apiSuccess (201: Created) {String} entry.author Author of the new book
+ * @apiSuccess (201: Created) {String} entry.date Publication date of the book
+ * @apiSuccess (201: Created) {Number} entry.isbn ISBN number of the book
+ * @apiSuccess (201: Created) {String} [entry.message] Optional message associated with the book
  *
- * @apiError (400: Name exists) {String} message "Name exists"
+ * @apiError (400: Bad Request) {String} message "Book with isbn already exists"
  * @apiError (400: Missing Parameters) {String} message "Missing required information - please refer to documentation"
- * @apiError (400: Invalid Priority) {String} message "Invalid or missing Priority  - please refer to documentation"
+ * @apiUse DBError
  */
 
 /**
- * @api {delete} /message/:name Request to remove an entry by name
+ * @api {put} /updateRating Request to change an entry
  *
- * @apiDescription Request to remove an entry associated with <code>name</code> in the DB
+ * @apiDescription Request to replace or change the rating of a book
  *
- * @apiName DeleteMessage
- * @apiGroup Message
+ * @apiName PutBookRating
+ * @apiGroup book
  *
- * @apiParam {String} name the name associated with the entry to delete
+ * @apiBody {String} title the title of the book entry
+ * @apiBody {String} author the author of the book entry
+ * @apiBody {number} rating the rating that the book rating will be updated to
+ * @apiBody {string} [message] An optional message or note associated with the book rating
  *
- * @apiSuccess {String} entry the string
- *      "Deleted: {<code>priority</code>} - [<code>name</code>] says: <code>message</code>"
+ * @apiSuccess (200: OK) {Object} entry Details of the updated book entry.
+ * @apiSuccess (200: OK) {String} entry.title The title of the updated book.
+ * @apiSuccess (200: OK) {String} entry.author The author of the updated book.
+ * @apiSuccess (200: OK) {Number} entry.rating The new rating of the book.
+ * @apiSuccess (200: OK) {String} [entry.message] The optional message associated with the book rating.
  *
- * @apiError (404: Name Not Found) {String} message "Name not found"
+ * @apiError (404: Not Found) {String} message "Book not found" if the specified book does not exist.
+ * @apiError (400: Bad Request) {String} message "Missing required information - please refer to documentation" if required parameters are missing.
+ * @apiUse DBError
  */
 
 /**
- * @api {put} /message Request to change an entry
+ * @api {delete} /deleteBook Request to delete a book by ISBN
  *
- * @apiDescription Request to replace the message entry in the DB for name
+ * @apiDescription Request to delete a specific book entry by providing its ISBN.
  *
- * @apiName PutMessage
- * @apiGroup Message
+ * @apiName DeleteBookByISBN
+ * @apiGroup book
  *
- * @apiBody {String} name the name entry
- * @apiBody {String} message a message to replace with the associated name
+ * @apiQuery {Number} isbn The ISBN of the book to be deleted.
  *
- * @apiSuccess {String} entry the string
- *      "Updated: {<code>priority</code>} - [<code>name</code>] says: <code>message</code>"
+ * @apiSuccess (200: OK) {String} message "Book with ISBN <code>isbn</code> has been successfully deleted."
  *
- * @apiError (404: Name Not Found) {String} message "Name not found"
- * @apiError (400: Missing Parameters) {String} message "Missing required information" *
+ * @apiError (404: Not Found) {String} message "Book not found" if no book with the specified ISBN exists.
+ * @apiError (400: Bad Request) {String} message "Invalid or missing ISBN - please refer to documentation" if the ISBN parameter is missing or invalid.
+ * @apiUse DBError
+ */
+
+/**
+ * @api {delete} /deleteBooks Request to delete a range of books
+ *
+ * @apiDescription Request to delete a range or series of book entries by specifying start and end dates or a range of ISBNs.
+ *
+ * @apiName DeleteBookByRange
+ * @apiGroup book
+ *
+ * @apiQuery {String} [startDate] The publication date to start deleting books from, in YYYY-MM-DD format.
+ * @apiQuery {String} [endDate] The publication date to delete books until, in YYYY-MM-DD format.
+ * @apiQuery {Number} [startISBN] The starting ISBN in the range to delete.
+ * @apiQuery {Number} [endISBN] The ending ISBN in the range to delete.
+ *
+ * @apiSuccess (200: OK) {String} message "Successfully deleted <code>count</code> books within the specified range."
+ *
+ * @apiError (404: Not Found) {String} message "No books found in the specified range" if no books match the given criteria.
+ * @apiError (400: Bad Request) {String} message "Invalid or missing range parameters - please refer to documentation" if neither a date range nor an ISBN range is provided or if the parameters are invalid.
+ * @apiUse DBError
  */
