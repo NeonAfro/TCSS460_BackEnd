0a8e47db438cb95335773b7f6bf299cbb69e5a8d
Jsbrenio <brenio.jeremiah.2021@gmail.com>
2024-10-29 14:20:14 -0700

    add book doc

---
+++ src/routes/closed/books/book.ts
@@ -0,0 +1,9 @@
+/**
+ * @api {get} /books Request to get all books
+ * @apiName books
+ * @apiGroup books
+ *
+ * @apiSuccess {Object[]} entry the books object containing information of the book <code>name</code>
+ *
+ * @apiError (500 Internal Server Error) {String} message "Internal Server Error"
+ */
