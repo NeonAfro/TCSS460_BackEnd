986c7cf45a2ca87dfcb9b7be5236216c202923ba
Jsbrenio <brenio.jeremiah.2021@gmail.com>
2024-10-29 14:40:30 -0700

    delete and modify file

--- src/routes/closed/books/book.ts
+++ src/routes/closed/books/book.ts
@@ -1,9 +1,21 @@
 /**
- * @api {get} /books Request to get all books
- * @apiName books
+ * @api {get} /books/all Request to get all books
+ * @apiName getAllBooks
  * @apiGroup books
  *
  * @apiSuccess {Object[]} entry the books object containing information of the book <code>name</code>
  *
  * @apiError (500 Internal Server Error) {String} message "Internal Server Error"
+ */
+
+/**
+ * @api {get} /books/:author Request to get all books by author
+ * @apiName getBooksByAuthor
+ * @apiGroup books
+ *
+ * @apiSuccess {Object[]} entry the books object containing information of the book from author <code>author</code>
+ * @apiParam {string} author the author to look up.
+ *
+ * @apiError (500 Internal Server Error) {String} message "Internal Server Error"
+ * @apiError (404: Name Not Found) {string} message "Author not found"
  */
