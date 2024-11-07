a064eefa5263b24dfc84a12350ef86ef1b155bff
Jeremiah <Breni.jeremiah.2021@gmail.com>
2024-11-06 22:07:24 -0800

    Fix Doc for book/all

--- src/routes/closed/book.ts
+++ src/routes/closed/book.ts
@@ -59,26 +59,27 @@ const format = (row) => ({
  * @apiName GetAllBooks
  * @apiGroup book
  *
- * @apiSuccess (200: OK) {Object[]} entry List of all book entries.
- * @apiSuccess (200: OK) {Number} entry.id Unique identifier of the book.
- * @apiSuccess (200: OK) {Object} entry.IBook Individual book entry.
- * @apiSuccess (200: OK) {Number} entry.IBook.isbn13 13-digit ISBN number of the book.
- * @apiSuccess (200: OK) {String} entry.IBook.authors List of authors of the book.
- * @apiSuccess (200: OK) {Number} entry.IBook.publication_year Year the book was published.
+ * @apiSuccess (200: OK) {Object[]} entries List of all book entries.
+ * @apiSuccess (200: OK) {Number} entries.id Unique identifier of the book.
+ * @apiSuccess (200: OK) {Object} entries.IBook Individual book entry.
+ * @apiSuccess (200: OK) {Number} entries.IBook.isbn13 13-digit ISBN number of the book.
+ * @apiSuccess (200: OK) {String} entries.IBook.authors List of authors of the book.
+ * @apiSuccess (200: OK) {Number} entries.IBook.publication_year Year the book was published.
  *
- * @apiSuccess (200: OK) {Object} entry.IBook.IRatings Ratings
- * @apiSuccess (200: OK) {Number} entry.IBook.IRatings.rating_avg Average rating of the book.
- * @apiSuccess (200: OK) {Number} entry.IBook.IRatings.rating_count Total ratings count.
- * @apiSuccess (200: OK) {Number} entry.IBook.IRatings.rating_1_star Count of 1-star ratings.
+ * @apiSuccess (200: OK) {Object} entries.IBook.IRatings Ratings
+ * @apiSuccess (200: OK) {Number} entries.IBook.IRatings.rating_avg Average rating of the book.
+ * @apiSuccess (200: OK) {Number} entries.IBook.IRatings.rating_count Total ratings count.
+ * @apiSuccess (200: OK) {Number} entries.IBook.IRatings.rating_1_star Count of 1-star ratings.
  *
- * @apiSuccess (200: OK) {Object} entry.IBook.IUrlIcon Icons
- * @apiSuccess (200: OK) {String} entry.IBook.IUrlIcon.image_url URL of the book's cover image.
- * @apiSuccess (200: OK) {String} entry.IBook.IUrlIcon.image_small_url Small image URL.
+ * @apiSuccess (200: OK) {Object} entries.IBook.IUrlIcon Icons
+ * @apiSuccess (200: OK) {String} entries.IBook.IUrlIcon.image_url URL of the book's cover image.
+ * @apiSuccess (200: OK) {String} entries.IBook.IUrlIcon.image_small_url Small image URL.
  *
- * @apiSuccess {Object} pagination metadata results from this paginated query
- * @apiSuccess {number} pagination.totalRecords the most recent count on the total amount of entries. May be stale.
- * @apiSuccess {number} pagination.limit the number of entry objects to returned.
- * @apiSuccess {number} pagination.cursor the value that should be used on a preceding call to this route.
+ * @apiSuccess (200: OK) {Object} pagination metadata results from this paginated query
+ * @apiSuccess (200: OK) {number} pagination.totalRecords the most recent count on the total amount of entries. May be stale.
+ * @apiSuccess (200: OK) {number} pagination.limit the number of entry objects to returned.
+ * @apiSuccess (200: OK) {number} pagination.offset the number used to offset the lookup of entry objects.
+ * @apiSuccess (200: OK) {number} pagination.nextPage the offset that should be used on a preceding call to this route.
  *
  * @apiuse ForbiddenJWT
  * @apiuse InvalidJWT
