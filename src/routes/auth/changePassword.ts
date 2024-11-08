0124557df932f49090751395c29df613d6b11fe3
Jsbrenio <brenio.jeremiah.2021@gmail.com>
2024-11-08 11:25:15 -0800

    Fixed Documentation on changePassword.ts

--- src/routes/auth/changePassword.ts
+++ src/routes/auth/changePassword.ts
@@ -16,7 +16,7 @@ import {
 export interface Auth {
     email: string;
     password: string;
-} 
+}
 
 const isStringProvided = validationFunctions.isStringProvided;
 const generateHash = credentialingFunctions.generateHash;
@@ -28,27 +28,26 @@ export interface IUserRequest extends Request {
     id: number;
 }
 
-
 const isValidNewPassword = (newPassword: string): boolean =>
     isStringProvided(newPassword) &&
     newPassword.length >= 8 &&
     newPassword.length <= 24 &&
     /[!@#$%^&*()_+=-]/.test(newPassword) &&
     /\d/.test(newPassword) &&
-    /[a-z]/.test(newPassword) && 
+    /[a-z]/.test(newPassword) &&
     /[A-Z]/.test(newPassword);
 
 /**
  * @api {put} /changePassword Request to create new password
  *
  * @apiDescription Request to create new password (known password)
- * 
+ *
  * <ul> <b>Password:</b>
  *      <li> Must be between 8 to 24 characters long</li>
  *      <li> Must include both uppercase and lowercase letters </li>
  *      <li> Must contain at least one numeric digit and special character </li>
  * </ul>
- * 
+ *
  *
  * @apiName PutChangePassword
  * @apiGroup Auth
@@ -57,51 +56,51 @@ const isValidNewPassword = (newPassword: string): boolean =>
  * @apiBody {String} password a users current password
  * @apiBody {String} newPassword a users new password
  * @apiBody {String} confirmNewPassword confirmation of new password
- * 
- * @apiSuccess (200: Success) {String} message "Password updated successfully"
- * 
- * @apiError (400: Missing Parameters) {String} message "Missing required information"
- * @apiError (400: Invalid Username) {String} message "Invalid Username - please refer to registration documentation"
+ *
+ * @apiSuccess (200: OK) {String} message "Password updated successfully"
+ *
+ * @apiError (400: Missing Parameters) {String} message "Missing a parameter"
+ * @apiError (400: Password Mismatch) {String} message "The passwords do not match"
  * @apiError (400: Invalid NewPassword) {String} message "Invalid New Password  - please refer to documentation"
- * @apiError (400: Invalid ConfirmPassword) {String} message "Invalid Confirmation Password  - please refer to documentation"
- * @apiError (400: Invalid OldPassword) {String} message "Password is not correct for User"
- * @apiError (404: User does not exist) {String} message "User does not exist in the Database"
+ * @apiError (404: User does not exist) {String} message "User does not exist"
+ * @apiError (400: Invalid OldPassword) {String} message "Old Password is not correct for User"
+ * @apiError (500: Server Error) {String} message "Server error - contact support"
  */
 changePasswordRouter.put(
     '/changePassword',
     (request: Request, response: Response, next: NextFunction) => {
-    //Verify that the caller supplied all the parameters
-    //In js, empty strings or null values evaulte to false
-    if ( // username, email, new password must be provided
-        isStringProvided(request.body.username) &&
-        isStringProvided(request.body.oldPassword) &&
-        isStringProvided(request.body.newPassword) &&
-        isStringProvided(request.body.confirmNewPassword)
-    ){
-        next();
-    } else{
-        response.status(400).send({
-            message: 'Missing required information',
-        })
-    }
+        //Verify that the caller supplied all the parameters
+        //In js, empty strings or null values evaulte to false
+        if (
+            // username, email, new password must be provided
+            isStringProvided(request.body.username) &&
+            isStringProvided(request.body.oldPassword) &&
+            isStringProvided(request.body.newPassword) &&
+            isStringProvided(request.body.confirmNewPassword)
+        ) {
+            next();
+        } else {
+            response.status(400).send({
+                message: 'Missing a parameter',
+            });
+        }
     },
     (request: Request, response: Response, next: NextFunction) => {
-        if(isValidNewPassword(request.body.newPassword)) {
-            if(request.body.newPassword == request.body.confirmNewPassword){
+        if (isValidNewPassword(request.body.newPassword)) {
+            if (request.body.newPassword == request.body.confirmNewPassword) {
                 next();
                 return;
             } else {
                 response.status(400).send({
-                    message:
-                        'The passwords do not match',
-                }); 
+                    message: 'The passwords do not match',
+                });
             }
             return;
         } else {
             response.status(400).send({
                 message:
                     'Invalid new password  - please refer to documentation',
-            }); 
+            });
             return;
         }
     },
@@ -112,7 +111,7 @@ changePasswordRouter.put(
         Account_Credential.account_id=Account.account_id 
         WHERE Account.username=$1`;
         const values = [request.body.username];
-    
+
         pool.query(theQuery, values)
             .then((result) => {
                 if (result.rows.length === 0) {
@@ -122,25 +121,30 @@ changePasswordRouter.put(
                     return;
                 } else if (result.rowCount > 1) {
                     //log the error
-                    console.error('DB Query error on sign in: too many result returned');
+                    console.error(
+                        'DB Query error on sign in: too many results returned'
+                    );
                     response.status(500).send({
                         message: 'server error - contact support',
                     });
                     return;
                 }
-    
+
                 // Retrieve the stored hash and salt
                 const { account_id, salted_hash, salt } = result.rows[0];
-                
+
                 // Verify the provided password with the stored hash and salt
-                const providedHash = generateHash(request.body.oldPassword, salt);
+                const providedHash = generateHash(
+                    request.body.oldPassword,
+                    salt
+                );
                 if (providedHash !== salted_hash) {
                     response.status(400).send({
-                        message: 'Invalid old password',
+                        message: 'Old Password is not correct for User',
                     });
                     return;
                 }
-    
+
                 // Password is correct, set the user id in the request and move to the next middleware
                 request.id = account_id;
                 next();
@@ -155,7 +159,9 @@ changePasswordRouter.put(
     },
     (request: IUserRequest, response: Response) => {
         if (!request.id) {
-            response.status(500).send({ message: 'Server error - contact support' });
+            response
+                .status(500)
+                .send({ message: 'Server error - contact support' });
             return;
         }
 
