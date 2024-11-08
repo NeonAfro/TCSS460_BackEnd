1ae77b81d5b05f0bda99779b8b8750539145045a
Jsbrenio <brenio.jeremiah.2021@gmail.com>
2024-11-08 15:49:06 -0800

    Allat

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -3,10 +3,9 @@ import express, { Router } from 'express';
 import { signinRouter } from './login';
 import { registerRouter } from './register';
 import { forgotPasswordRouter } from './forgotPassword';
-import { changePasswordRouter } from './changePassword';
 
 const authRoutes: Router = express.Router();
 
-authRoutes.use(signinRouter, registerRouter, forgotPasswordRouter, changePasswordRouter);
+authRoutes.use(signinRouter, registerRouter, forgotPasswordRouter);
 
 export { authRoutes };
--- src/routes/closed/index.ts
+++ src/routes/closed/index.ts
@@ -3,10 +3,12 @@ import express, { Router } from 'express';
 import { checkToken } from '../../core/middleware';
 import { tokenTestRouter } from './tokenTest';
 import { bookRouter } from './book';
+import { changePasswordRouter } from './changePassword';
 
 const closedRoutes: Router = express.Router();
 
 closedRoutes.use('/jwt_test', checkToken, tokenTestRouter);
 closedRoutes.use('/book', checkToken, bookRouter);
+closedRoutes.use(checkToken, changePasswordRouter);
 
 export { closedRoutes };
